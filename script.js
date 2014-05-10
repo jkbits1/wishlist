5bcdecc5466788d737bd1125de2742c224f88be3
jkbits1 <jkbits1@users.noreply.github.com>
2014-05-10 18:48:03 +0100

    add wishes

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 /* Exercise 1: Wish list */
+
+$(document).ready(function(){
+
+
+
+    $(document).on('click', "#add-to-list", function() {
+
+        var item = $('#item').val();
+
+        addToList(item);
+
+//        alert("hi");
+//        alert($('#item').val());
+    });
+
+});
+
+function addToList(item) {
+
+    var newElem = "<li>" + item + "</li>";
+
+    $("ol#items").append(newElem);
+
+}
+
